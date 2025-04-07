039d34db1d9a8a2d79208f054c742ffc561cc025
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-04-07 21:02:27 +0400

    msg

--- src/app/Models/user.ts
+++ src/app/Models/user.ts
@@ -1,10 +1,9 @@
 
 
 export class User {
-    id?: number;
-    email!: string;
-    first_name!: string;
-    last_name!: string;
-    avatar!:  string;
+    id? : number
+    email? : string
+    firstName? : string
+    lastName? : string
+    avatar? : string
 }
-
